66fd819e2bd644855cf7fb4084ddf6dd2a48ed47
Gootjes <f.c.gootjes@student.rug.nl>
2016-08-24 00:44:49 +0200

    working straighttrack

--- objects.py
+++ objects.py
@@ -6,6 +6,7 @@ from util import Point
 
 
 import math as  T
+import operator
 
 class Track(serialization.Serializable):
 
@@ -23,7 +24,7 @@ class StraightTrack(Track):
         self.set_location(location)
 
     def set_location(self, point):
-        self.location=location
+        self.location=point
         self.update()
 
     def get_location(self):
@@ -36,13 +37,85 @@ class StraightTrack(Track):
         halfway=self.length/2
         x=T.cos(T.radians(self.rotation))*halfway
         z=T.sin(T.radians(self.rotation))*halfway
-        self._l1=self.location.subtract(x, 0, z)
-        self._l2=self.location.add(x, 0, z)
+        self._l1=self.location.apply(x, 0, z, func=operator.sub)
+        self._l2=self.location.apply(x, 0, z, func=operator.add)
 
-    def compute_move(self, from_point, distance, D=True):
-        if D:
-            distance_point=self._l2.subtract(from_point)
-            distance_left=T.cos(self.rotation)*distance
-            
-            
+    def compute_move(self, offset, distance, D=True):
+        reach=distance+offset
+        a=self._l2 if D else self._l1
+        df=self.length-reach
+        if df < 0:
+            return a, df*-1
+        else:
+            off=0 if D else 180
+            x=T.cos(T.radians(self.rotation+off))*df
+            z=T.sin(T.radians(self.rotation+off))*df
+            return a.apply(func=operator.sub, x=x, y=0, z=z), 0
 
+    def point_to_offset(self, point, D=True):
+        pass
+            
+    def compute_move_(self, from_point, distance, D=True):
+        pass
+        
+class CurveTrack(Track):
+
+    def __init__(self, rotation=0, location=Point(0,0,0), radius=0, angle=360/16.0):
+        Track.__init__(self, rotation)
+        self.radius=radius
+        self.angle=angle
+        self.length=self._get_length()
+        self.set_location(location)
+
+    def _get_length(self):
+        return self._calc_distance_on_curve(self.angle)
+
+    def set_location(self,point):
+        self.location=point
+        self.update()
+
+    def update(self):
+        self.center_point=self._get_center_point()
+        self._compute_locs()
+
+    def _compute_locs(self):
+        self._l1=self.center_point.apply(T.cos(T.radians(self.rotation))*self.radius, 0, T.sin(T.radians(self.rotation))*self.radius, func=operator.add)
+        self._l2=self.center_point.apply(T.cos(T.radians(self.rotation+self.angle))*self.radius, 0, T.sin(T.radians(self.rotation+self.angle))*self.radius, func=operator.add)
+
+    def _get_center_point(self):
+        x=T.cos(T.radians(self.rotation+180+self.angle/2.0))*self.radius
+        z=T.sin(T.radians(self.rotation+180+self.angle/2.0))*self.radius
+        return self.location.apply(func=operator.add, x=x, y=0, z=z)
+
+    def _calc_distance_on_curve(self, degrees):
+        return 2*self.radius*T.pi*degrees/360.0
+    
+    def _calc_degrees_change(self, distance):
+        return distance*360/(2*self.radius*T.pi)
+
+    def compute_move(self, offset, distance, D=True):
+        reach=offset+distance
+        a=self._l2 if D else self._l1
+        df=self.length-reach
+        if df < 0:
+            return a, df
+        else:
+            dc=self._calc_degrees_change(reach)
+            dco=self.rotation+(dc-(self.angle/2)) if D else self.rotation-(dc+(self.angle/2))
+            return self.center_point.apply(T.cos(T.radians(dco))*self.radius, 0, T.sin(T.radians(dco))*self.radius, func=operator.add), 0
+        
+    
+
+if __name__=="__main__":
+    #Test battery
+
+    s=StraightTrack(length=10)
+    res2=s.compute_move(0,3)
+
+    s1=StraightTrack(90, length=10)
+    res_12=s1.compute_move(0,3)
+
+    s2=StraightTrack(292.5, length=10)
+    res_22=s2.compute_move(0,3)
+
+    res_2=s.compute_move(0,3,False)
--- util.py
+++ util.py
@@ -2,6 +2,8 @@ from __future__ import division
 
 from collections import namedtuple
 
+import operator
+
 #Point=namedtuple('Point','x y z')
 
 class Point(object):
@@ -14,20 +16,8 @@ class Point(object):
     def __repr__(self):
         return "Point(%s=%f,%s=%f,%s=%f)" % ('x', self.x, 'y', self.y, 'z', self.z)
 
-    def _apply(self, x=0, y=0, z=0, subtract=False):
-        if not subtract:
-            return Point(self.x+x,self.y+y,self.z+z)
-        else:
-            return Point(self.x-x,self.y-y,self.z-z)
+    def apply(self, x=0, y=0, z=0, func=operator.add, point=None):
+        if point:
+            x,y,z=point.x,point.y,point.z
+        return Point(func(self.x, x),func(self.y, y),func(self.z, z))
         
-    def subtract_point(self, point):
-        return self.subtract(point.x, point.y, point.z)
-
-    def subtract(self, x=0,y=0,z=0):
-        return self._apply(x,y,z,True)
-
-    def add_point(self, point):
-        return self.add(point.x, point.y, point.z)
-
-    def add(self, x=0, y=0, z=0):
-        return self._apply(x,y,z)
